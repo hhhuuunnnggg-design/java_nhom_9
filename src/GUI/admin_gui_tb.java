591cf756a8a43b4d023ae25eca240742f37c4939
Phuong Anh Nguyen <alixblu3011@gmail.com>
2024-05-17 12:51:29 +0700

    new db update

---
+++ src/GUI/admin_gui_tb.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+public class admin_gui_tb {
+    JFrame f;
+
+    public admin_gui_tb() {
+        f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new BorderLayout());
+        f.setSize(850, 670);
+        f.setLocationRelativeTo(null);
+                f.setResizable(false);
+        
+        
+    }
+}
